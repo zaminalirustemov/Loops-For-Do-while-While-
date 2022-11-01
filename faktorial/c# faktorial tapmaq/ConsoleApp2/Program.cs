1d481d32a90ca26548b7ccb9b60fcfa4824c762d
zaminalirustemov <2g17rck@code.edu.az>
2022-11-01 12:46:43 +0400

    faktorial tapmaq

---
+++ faktorial/c# faktorial tapmaq/ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, fact = 1, number;
+            Console.Write("Eded daxil edin: ");
+            number = int.Parse(Console.ReadLine());
+            for (i = 1; i <= number; i++)
+            {
+                fact = fact * i;
+            }
+            Console.Write(number + " != " + fact);
+        }
+    }
+}
