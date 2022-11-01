2530e6fa80874c63a20e0a4baed20397a6cc2a06
zaminalirustemov <2g17rck@code.edu.az>
2022-11-01 12:50:04 +0400

    sademurekkeb

---
+++ sade murekkeb/c#prime or composite/prime or composite/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace prime_or_composite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, i, m = 0, a = 0;
+            Console.Write("Zehmet olmasa ededinizi daxil edin: ");
+            n = int.Parse(Console.ReadLine());
+            m = n / 2;
+            for (i = 2; i <= m; i++)
+            {
+                if (n % i == 0)
+                {
+                    Console.Write("Sizin edediniz olduqca murekkebdir.");
+                        a = 1;
+                    break;
+                }
+            }
+            if (a == 0)
+                Console.Write("Edediniz sadedir.");
+        }
+    }
+}
