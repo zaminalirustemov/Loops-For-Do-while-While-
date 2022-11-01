7a7147286a8deee86d5e44aad474fd139c372de4
zaminalirustemov <2g17rck@code.edu.az>
2022-11-01 12:52:03 +0400

    mertebe

---
+++ mertebe/c#ededin mertebesi/ededin mertebesi/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ededin_mertebesi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Eded daxil ediniz: ");
+            int i = int.Parse(Console.ReadLine());
+            int counter = 0;
+            while (i > 0)
+            {
+                i /= 10;
+                counter++;
+            }
+            Console.WriteLine("Sizin edediniz " + counter + " mertebelidir.");
+        }
+    }
+}
